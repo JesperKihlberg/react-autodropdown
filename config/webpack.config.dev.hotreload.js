22dc03a5763f19f1501922ed18758097bc40fa97
Jesper Kihlberg <jesper.kihlberg@edlund.dk>
2019-02-27 08:43:39 +0100

    Use older version of css-loader

--- config/webpack.config.dev.hotreload.js
+++ config/webpack.config.dev.hotreload.js
@@ -3,24 +3,24 @@
 let path = require("path");
 let webpack = require("webpack");
 let WebpackConfig = require("webpack-config");
-module.exports = new WebpackConfig.Config().extend({
-  "config/webpack.config.app.js": config => {
-    config.module.rules.filter(l => l.loader == "babel-loader");
-    config.output.filename = "[name].js";
-    return config;
-  }
-}).merge({
-  devtool: "cheap-module-eval-source-map",
-  entry: {
-    app: [
-      "eventsource-polyfill", // necessary for hot reloading with IE
-      "webpack-hot-middleware/client",
-    ]
-  },
-  output: {
-    path: path.resolve("../../../../Debug/webclient-hotreload/js"),
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-});
+module.exports = new WebpackConfig.Config()
+  .extend({
+    "config/webpack.config.app.js": config => {
+      config.module.rules.filter(l => l.loader == "babel-loader");
+      config.output.filename = "[name].js";
+      return config;
+    }
+  })
+  .merge({
+    devtool: "cheap-module-eval-source-map",
+    entry: {
+      app: [
+        "eventsource-polyfill", // necessary for hot reloading with IE
+        "webpack-hot-middleware/client"
+      ]
+    },
+    output: {
+      path: path.resolve("../../../../Debug/webclient-hotreload/js")
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+  });
